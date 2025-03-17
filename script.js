ce01149c35f60ca919444c7c40fc6b67ab1991c0
strom314 <jakub.gros777@gmail.com>
2025-03-17 11:04:42 +0100

    for loop creates divs and another adds them to container

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const container = document.querySelector("#container");
+const canvasDivs = [];
+
+for (let i = 0; i < 16; i++) {
+    let canvasDiv = document.createElement("div");
+
+    canvasDiv.style.backgroundColor = "blue";
+    canvasDiv.classList = "canvas-square";
+
+    canvasDivs.push(canvasDiv);
+}
+
+canvasDivs.forEach(div => {
+    container.appendChild(div);
+});
